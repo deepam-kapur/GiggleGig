574d6b61de4fe3262b5310d65282356520f61d89
Deepam Kapur <deepamkapur@gmail.com>
2024-08-24 00:40:14 +0530

    [winston]

--- src/common/middleware.ts
+++ src/common/middleware.ts
@@ -1,4 +1,6 @@
 import type { NextFunction, Request, Response } from 'express';
+import type { Format } from 'logform';
+import { createLogger, format, transports } from 'winston';
 
 import config from '../config';
 
@@ -25,20 +27,58 @@ const checkToken = (req: Request, res: Response, next: NextFunction): void => {
   next();
 };
 
+const customLevels = {
+  levels: {
+    error: 0,
+    warn: 1,
+    info: 2,
+    debug: 3,
+  },
+  colors: {
+    error: 'red',
+    warn: 'yellow',
+    info: 'green',
+    debug: 'blue',
+  },
+};
+
+const customFormat: Format = format.printf(({ level, message, timestamp }) => {
+  return `${timestamp} [${level.toUpperCase()}]: ${message}`;
+});
+
+const logger = createLogger({
+  levels: customLevels.levels,
+  format: format.combine(
+    format.colorize(),
+    format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+    customFormat,
+  ),
+  transports: [
+    new transports.Console(),
+    new transports.File({ filename: 'combined.log' }),
+  ],
+});
+
 const logMiddleware = (
   req: Request,
   res: Response,
   next: NextFunction,
 ): void => {
-  console.log({
-    url: req.url,
-    body: req.body,
-    params: req.params,
-    query: req.query,
-    method: req.method,
-    headers: req.headers,
-    timestamp: new Date().toISOString(),
-  });
+  logger.info(
+    `logMiddleware - ${JSON.stringify(
+      {
+        url: req.url,
+        body: req.body,
+        params: req.params,
+        query: req.query,
+        method: req.method,
+        headers: req.headers,
+        timestamp: new Date().toISOString(),
+      },
+      null,
+      4,
+    )}`,
+  );
 
   next();
 };
