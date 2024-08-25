782bc8753ab4f6646704a6879e98fb618645f7d6
Deepam Kapur <deepamkapur@gmail.com>
2024-08-25 15:49:29 +0530

    [Add] slack subscribe redirect enabled

--- src/api/controllers/slack.subscribe.controller.ts
+++ src/api/controllers/slack.subscribe.controller.ts
@@ -1,17 +1,21 @@
 import type { Request, Response } from 'express';
 
-import response from '../../common/response';
 import slackService from '../../service/slack.service';
 
 export default async (req: Request, res: Response) => {
   try {
     const { code } = req.query;
+    if (!code) {
+      // Construct the new URL
+      const redirectUrl = `https://web.gigglegig.fun/error`;
+
+      // Redirect the request
+      return res.redirect(301, redirectUrl);
+    }
 
     if (code) {
       await slackService.authorizeBot(code as string);
     }
-
-    response(res);
 
     // Construct the new URL
     const redirectUrl = `https://web.gigglegig.fun/installed`;
